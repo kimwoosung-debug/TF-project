cbe4a105c63e6ffb735cbbd1d671fa3d8abd0052
kimwoosung-debug <rladntd9942@naver.com>
2023-09-08 20:11:49 +0900

    feat: security group and alb resource provisioner / add: Add module for ecs cluster

---
+++ data.tf
@@ -0,0 +1,13 @@
+data "aws_vpc" "selected" {
+  tags = {
+    Name = "sample-vpc"
+  }
+}
+
+data "aws_subnets" "public" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.selected.id]
+  }
+}
+
